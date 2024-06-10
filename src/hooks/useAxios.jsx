6d1e7ce7015109176e67306dd488ad0e7a4918e4
Fabian Roy <fabianroylikhon@gmail.com>
2024-06-10 15:53:32 +0600

    Added Axios

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,14 @@
+
+const axiosSecure = axios.create({
+    baseURL: 'http:/localhost:3000',
+});
+
+const useAxios = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default useAxios;
