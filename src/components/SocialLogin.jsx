196564d2a6aa16e91e2a61dc55453521e8c8b940
Fabian Roy <fabianroylikhon@gmail.com>
2024-06-10 20:49:14 +0600

    Added Google Login System

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,8 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import usePublicAxios from "../hooks/usePublicAxios";
+import useAuth from "../hooks/useAuth";
+import Swal from "sweetalert2";
+import { FaGoogle } from "react-icons/fa";
 
 const SocialLogin = () => {
+  
+    const { signWithGoogle } = useAuth();
+    const axiosPubic = usePublicAxios();
+
+    const navigate = useNavigate();
+
+    const handleWithGoogle = () => {
+        signWithGoogle()
+            .then(result => {
+                console.log(result.user);
+                const userInfo = {
+                    email: result.user.email,
+                    name: result.user.displayName,
+                    photoURL: result.user.photoURL,
+                }
+                axiosPubic.post('/users', userInfo)
+                    .then(res => {
+                        console.log(res.data);
+                        navigate('/');
+                        Swal.fire({
+                            title: 'Success',
+                            text: 'User login successfully',
+                            icon: 'success'
+                        });
+                    })
+            })
+    }
+
     return (
-        <div>
-            
+        <div className="w-fit mx-auto">
+            <h2 className="text-center mb-2 font-semibold text-lg">Or</h2>
+            <button onClick={handleWithGoogle} className="btn">
+                <FaGoogle />
+                <span>Sign with Google</span>
+            </button>
         </div>
     );
 };
